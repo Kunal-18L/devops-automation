cdc4407f4a02c44a07ecd7b11a925abc2a6f6972
Ubuntu <ubuntu@ip-172-31-2-143.ap-south-1.compute.internal>
2026-07-15 07:06:28 +0000

    Kubernetes role added

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,20 @@ resource "aws_instance" "terraform_demo" {
     Name = var.instance_name
   }
 }
+
+
+resource "aws_instance" "kubernetes_server" {
+  ami                    = var.ami_id
+  instance_type          = "c7i-flex.large"
+  key_name               = "aws-linux"
+  vpc_security_group_ids = [aws_security_group.terraform_sg.id]
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "kubernetes-server"
+  }
+}
